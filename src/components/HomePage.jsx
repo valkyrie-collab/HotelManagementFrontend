eb4aa55872ea67f69f6d7857d7870e20f1fe9760
valkyrie <rajarshibiswas5678@gmail.com>
2025-10-05 10:42:51 +0000

    date tweaking in the home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -60,7 +60,8 @@ function HomePage() {
                 setDecode(data);
                 console.log("the decoded token is", data);
 
-                if (data.iat === data.exp) {
+                if (new Date() >= new Date(data.exp)) {
+                    localStorage.clear();
                     setIsExpired(true);
                 }
 
