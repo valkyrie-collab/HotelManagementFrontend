c23f16291b132d03ac290d3192e2fd77b83374e7
valkyrie <rajarshibiswas5678@gmail.com>
2025-10-26 00:04:42 +0530

    updated vite.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: [
+      'localhost',
+      'nongalvanized-letisha-unrifted.ngrok-free.dev'
+    ]
+  }
 })
